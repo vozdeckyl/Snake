0a700cb0bc2c7813fb42eaca764c47b428fb7f39
Lubos Vozdecky <vozdeckyl@gmail.com>
2021-10-09 19:58:01 +0100

    maximizing window

--- src/main.cpp
+++ src/main.cpp
@@ -109,8 +109,10 @@ void records()
 
 int main()
 {
+    system("wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz");
     cout << "Snake v" << Snake_VERSION_MAJOR << "." << Snake_VERSION_MINOR << " " << BUILD << endl;
-
+    system("sleep 1");
+    
     FileManager::addFile("logo",std::vector<std::string>({"../data/logo.txt","/usr/local/share/shellsnake/logo.txt"}));
     FileManager::addFile("settings",std::vector<std::string>({"../data/settings.bin","/var/shellsnake/settings.bin"}));
     FileManager::addFile("scores",std::vector<std::string>({"../data/scores.bin","/var/shellsnake/scores.bin"}));
