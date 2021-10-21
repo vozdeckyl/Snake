910ede88d31114e2f823135d5400d89ba337ffc4
Lubos Vozdecky <vozdeckyl@gmail.com>
2021-10-21 22:51:21 +0100

    testing menu for colors

--- test/menuTest.cpp
+++ test/menuTest.cpp
@@ -30,6 +30,21 @@ TEST(menuTest, drawing)
     ASSERT_TRUE(engine->testScreen('>',1,0));
     ASSERT_TRUE(engine->testScreen('<',1,2));
     
+    
+    ASSERT_TRUE(engine->testTextColor(Color::white,1,0));
+    ASSERT_TRUE(engine->testTextColor(Color::white,1,2));
+    ASSERT_TRUE(engine->testTextColor(Color::white,1,1));
+    ASSERT_TRUE(engine->testTextColor(Color::white,2,1));
+    ASSERT_TRUE(engine->testTextColor(Color::white,3,1));
+    ASSERT_TRUE(engine->testTextColor(Color::white,4,1));
+
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,1,0));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,1,2));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,1,1));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,2,1));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,3,1));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,4,1));
+    
     delete engine;
     delete menu;
 }
@@ -49,43 +64,84 @@ TEST(menuTest, selection)
     ASSERT_TRUE(engine->testScreen('>',1,0));
     ASSERT_TRUE(engine->testScreen('<',1,2));
     ASSERT_TRUE(menu->getResult()==0);
-    
+
+    engine->clearScreen();
     menu->moveSelectorDown();
     menu->draw(engine);
 
+    ASSERT_TRUE(engine->testScreen(' ',1,0));
+    ASSERT_TRUE(engine->testScreen(' ',1,2));
     ASSERT_TRUE(engine->testScreen('>',2,0));
     ASSERT_TRUE(engine->testScreen('<',2,2));
     ASSERT_TRUE(menu->getResult()==1);
     
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,1,0));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,1,2));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,1,1));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,2,0));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,2,2));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,2,1));
+    
+    engine->clearScreen();
     menu->moveSelectorDown();
     menu->draw(engine);
-    
+
+    ASSERT_TRUE(engine->testScreen(' ',2,0));
+    ASSERT_TRUE(engine->testScreen(' ',2,2));
     ASSERT_TRUE(engine->testScreen('>',3,0));
     ASSERT_TRUE(engine->testScreen('<',3,2));
     ASSERT_TRUE(menu->getResult()==2);
 
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,2,0));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,2,2));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,2,1));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,3,0));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,3,2));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,3,1));
+    
+    engine->clearScreen();
     menu->moveSelectorDown();
     menu->draw(engine);
-	
-    ASSERT_TRUE(engine->testScreen('>',4,0));
-    ASSERT_TRUE(engine->testScreen('<',4,2));
-    ASSERT_TRUE(menu->getResult()==3);
-    
-    menu->moveSelectorDown();
-    menu->draw(engine);
-    
+
+    ASSERT_TRUE(engine->testScreen(' ',3,0));
+    ASSERT_TRUE(engine->testScreen(' ',3,2));
     ASSERT_TRUE(engine->testScreen('>',4,0));
     ASSERT_TRUE(engine->testScreen('<',4,2));
     ASSERT_TRUE(menu->getResult()==3);
 
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,3,0));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,3,2));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,3,1));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,4,0));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,4,2));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,4,1));
+    
+    engine->clearScreen();
+    menu->moveSelectorDown();
+    menu->draw(engine);
 
+    ASSERT_TRUE(engine->testScreen(' ',3,0));
+    ASSERT_TRUE(engine->testScreen(' ',3,2));
+    ASSERT_TRUE(engine->testScreen('>',4,0));
+    ASSERT_TRUE(engine->testScreen('<',4,2));
+    ASSERT_TRUE(menu->getResult()==3);
+
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,3,0));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,3,2));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::black,3,1));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,4,0));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,4,2));
+    ASSERT_TRUE(engine->testBackgroundColor(Color::red,4,1));
+
+    engine->clearScreen();
     menu->moveSelectorUp();
     menu->draw(engine);
     
     ASSERT_TRUE(engine->testScreen('>',3,0));
     ASSERT_TRUE(engine->testScreen('<',3,2));
     ASSERT_TRUE(menu->getResult()==2);
-    
+
+    engine->clearScreen();
     menu->moveSelectorUp();
     menu->draw(engine);
 
@@ -93,13 +149,15 @@ TEST(menuTest, selection)
     ASSERT_TRUE(engine->testScreen('<',2,2));
     ASSERT_TRUE(menu->getResult()==1);
 
+    engine->clearScreen();
     menu->moveSelectorUp();
     menu->draw(engine);
 
     ASSERT_TRUE(engine->testScreen('>',1,0));
     ASSERT_TRUE(engine->testScreen('<',1,2));
     ASSERT_TRUE(menu->getResult()==0);
-    
+
+    engine->clearScreen();
     menu->moveSelectorUp();
     menu->draw(engine);
 
